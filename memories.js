9a3ecc00b87e84d7e4413bb4aeba99ebedcef216
Sabil Mukhi <sabilmukhi6996@gmail.com>
2019-10-18 01:25:54 +0930

    make-up
    Signed-off-by: Sabil Mukhi <sabilmukhi6996@gmail.com>

---
+++ memories.js
@@ -0,0 +1,60 @@
+
+function openNav() {
+document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+document.getElementById("mySidenav").style.width = "0";
+}
+
+
+var i = 0; 			// Start Point
+var images = [];	// Images Array
+var time = 1000;	// Time Between Switch
+
+// Image List
+images[0] = "IMAGES/1memories.jpg";
+images[1] = "IMAGES/2memories.jpg";
+images[2] = "IMAGES/3memories.jpg";
+images[3] = "IMAGES/4memories.jpg";
+
+// Change Image
+function changeImg(){
+	document.slide.src = images[i];
+
+	// Check If Index Is Under Max
+	if(i < images.length - 1){
+	  // Add 1 to Index
+	  i++;
+	} else {
+		// Reset Back To O
+		i = 0;
+	}
+
+	// Run function every x seconds
+	setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload=changeImg;
+
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
