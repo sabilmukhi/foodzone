90709b3ca1ef5a9aa3bc15ba42fc0de11fd53efd
Sabil Mukhi <sabilmukhi6996@gmail.com>
2019-10-17 21:50:05 +0930

    final make-up
    Signed-off-by: Sabil Mukhi <sabilmukhi6996@gmail.com>

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+
+function openNav() {
+document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+document.getElementById("mySidenav").style.width = "0";
+}
+
 var i = 0; 			// Start Point
 var images = [];	// Images Array
 var time = 1000;	// Time Between Switch
